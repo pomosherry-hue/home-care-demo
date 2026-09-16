58e08743390e6784d74b091184651404f9882af5
pomosherry-hue <pomosherry@gmail.com>
2026-09-16 12:54:04 +0800

    Connect apps to Firebase Realtime Database

---
+++ firebase-config.js
@@ -0,0 +1,29 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAiA_NoTpR2iVSm8zfKfTkMl2vHdpaMl_U",
+  authDomain: "home-care-4dbb2.firebaseapp.com",
+  databaseURL: "https://home-care-4dbb2-default-rtdb.firebaseio.com",
+  projectId: "home-care-4dbb2",
+  storageBucket: "home-care-4dbb2.firebasestorage.app",
+  messagingSenderId: "822062174005",
+  appId: "1:822062174005:web:fdac0440a465a69866467c",
+  measurementId: "G-14108DRHL4"
+};
+
+window.ELDER_DEMO = {
+  id: "elder_001",
+  name: "张爷爷",
+  age: 89,
+  address: "朝阳区 XXX小区",
+  ability: {
+    "吃饭": "自己可以",
+    "喝水": "需要一点帮助",
+    "翻身": "需要较多帮助",
+    "坐起": "需要较多帮助",
+    "站立": "完全需要帮助",
+    "转移": "完全需要帮助",
+    "如厕": "需要较多帮助",
+    "穿衣": "需要较多帮助",
+    "洗澡": "完全需要帮助",
+    "沟通": "自己可以"
+  }
+};
